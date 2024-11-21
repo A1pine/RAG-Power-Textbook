f9c78b8d2074bde8e24e80e202b7d796bcb8c3e7
A1pine <me@xuning.eu.org>
2024-11-22 00:17:32 +0800

    修复  openai API 变更导致的报错: TypeError: 'ChatCompletionMessage' object is not subscriptable

--- rag_evaluator/test_case_generator.py
+++ rag_evaluator/test_case_generator.py
@@ -21,7 +21,7 @@ class TestCaseGenerator:
                       {"role": "user", "content": prompt}],
             temperature=0.7
         )
-        return response.choices[0].message['content']
+        return response.choices[0].message.content
 
 if __name__ == "__main__":
     sample_context = """
